b7a734e637d0b48e13ddbfc495380c0834d2b9db
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2025-02-19 22:45:19 +0530

    Add files via upload

---
+++ Number_pattern.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Number_pattern
+{
+    public static void main(String[] args)
+    {
+        int a = 1;
+        for (int i =0; i <= 10; i++)
+        {
+            for(int j = i; j<=i ; j++)
+            {
+                System.out.println(j);
+            }
+        }
+        System.out.println();
+
+    }
+}
